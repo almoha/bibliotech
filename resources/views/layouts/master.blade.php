d3651ac26864db9eb731b85faa243f5a01fbeab1
almoha <almoha1975@gmail.com>
2019-05-13 22:06:28 +0200

    Paramétrage logout et couleur sur fontawesome

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -76,7 +76,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         <li class="nav-item">
                             <router-link to="/dashboard" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <i class="nav-icon fas fa-tachometer-alt color-blue"></i>
                                 <p>
                                     Dashboard
                                 </p>
@@ -85,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         <li class="nav-item has-treeview">
                             <a href="#" class="nav-link">
-                                <i class="nav-icon fa fa-cog"></i>
+                                <i class="nav-icon fa fa-cog color-green"></i>
                                 <p>
                                     Management
                                     <i class="right fa fa-angle-left"></i>
@@ -109,21 +109,30 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         <li class="nav-item">
                             <router-link to="profile" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-user color-orange"></i>
                                 <p>
                                     Profile
                                 </p>
                             </router-link>
                         </li>
-
+                                
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fa fa-power-off"></i>
+                            <a class="nav-link" href="{{ route('logout') }}" 
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                                <i class="nav-icon fa fa-power-off color-red"></i>
                                 <p>
                                     Logout
                                 </p>
                             </a>
-                        </li>                        
+                        
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>       
+
+
+
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
